52243cf7b6a4dbcefb400212cc87768b277050fb
Danny Irwin <danny@libertinegames.com>
2020-03-02 17:34:26 -0800

    Added test sounds and button to play sounds

--- WumpusTest/WumpusTest/Form1.cs
+++ WumpusTest/WumpusTest/Form1.cs
@@ -12,9 +12,27 @@ namespace WumpusTest
 {
     public partial class Form1 : Form
     {
+        private string textBoxValue;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxValue))
+            {
+                SoundLibrary soundLibrary = new SoundLibrary();
+                soundLibrary.PlaySound(textBoxValue);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBoxValue = textBox1.Text;
+            }
         }
     }
 }
--- WumpusTest/WumpusTest/SoundLibrary.cs
+++ WumpusTest/WumpusTest/SoundLibrary.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace WumpusTest
 {
     class SoundLibrary
     {
+        private string soundsPath = @"C:\Users\Brogan\Source\Repos\WumpusTest\WumpusTest\WumpusTest\Sounds\";
+        private SoundPlayer soundPlayer;
+        public void PlaySound(string name)
+        {
+            string path = soundsPath + name;
+            soundPlayer = new SoundPlayer();
+            soundPlayer.SoundLocation = path;
+            soundPlayer.LoadCompleted += SoundPlayer_LoadCompleted;
+            soundPlayer.Load();
+        }
+
+        private void SoundPlayer_LoadCompleted(object sender, EventArgs e)
+        {
+            soundPlayer.Play();
+        }
     }
 }
